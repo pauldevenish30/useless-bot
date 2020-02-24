fae2ef3765a8820d280365d105fd12d13f4f18ae
P.A.Devenish <pad30@kent.ac.uk>
2020-02-24 15:01:24 +0000

    Updated bot.js to cover tea and biscuits

--- sample/bot.js
+++ sample/bot.js
@@ -1,7 +1,7 @@
 function answerQuery(query) {
   query = query.toLowerCase();
-  if (query.indexOf("rain") != -1 || query.indexOf("sun") != -1 || query.indexOf("weather") != -1)
-    return "I do not care too much about weather, I'm locked inside a data center.";
+  if (query.indexOf("tea") != -1 || query.indexOf("biscuits") != -1)
+    return "I like to consume tea or biscuits.";
   return "Sorry Dave, I cannot do that."
 }
 
@@ -11,7 +11,7 @@ function handleSayClick() {
   // which we are using for showing all the conversation history
   var message = document.getElementById('message').value.toLowerCase();
   var conversation = document.getElementById('conversation');
-  
+
   // Get answer for the given query and show the query & answer
   var answer = answerQuery(message)
   conversation.innerHTML += "<p><strong>You:</strong> " + message + "</p>";
